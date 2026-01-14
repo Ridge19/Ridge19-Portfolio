f116d562ed278d8f160ce9507ca6cba3ea14c465
Ridge19 <Riidgyy@gmail.com>
2026-01-14 18:52:40 +1100

    created production build to deploy website

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -7,7 +7,7 @@ import 'react-vertical-timeline-component/style.min.css';
 import '../assets/styles/Timeline.scss'
 
 function Timeline() {
-  return (
+  return ( 
     <div id="history">
       <div className="items-container">
         <h1>Career History</h1>
